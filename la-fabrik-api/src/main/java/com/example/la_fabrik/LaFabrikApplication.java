e8e3eda0fdddaebf10c23f6cf67eab70839d2c41
Althaf Azeez <althafazeez@althafs-mbp.mynetworksettings.com>
2024-07-11 20:03:47 -0400

    created api

---
+++ la-fabrik-api/src/main/java/com/example/la_fabrik/LaFabrikApplication.java
@@ -0,0 +1,13 @@
+package com.example.la_fabrik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFabrikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFabrikApplication.class, args);
+	}
+
+}
---
+++ la-fabrik-api/src/test/java/com/example/la_fabrik/LaFabrikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.la_fabrik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaFabrikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
